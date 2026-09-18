96c53190a341e00638e1d8cdbbb24eb50dea95fd
CaRToN109 <anto.drean@gmail.com>
2026-09-18 12:59:32 +0200

    created prisma service and injected it into app.service, need to update prisma usage in every other service.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { HealthController } from './health.controller';
 import { AuthController } from './controller';
 import { PlaylistController } from './playlist.controller';
 import { PlayerModule } from './player/player.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PlayerModule],
+  imports: [PlayerModule, PrismaModule],
   controllers: [
     AppController,
     HealthController,
